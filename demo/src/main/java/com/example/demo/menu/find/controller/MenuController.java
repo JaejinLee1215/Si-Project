4fcdb83842c54645d519a28654001a73f35d2988
JaejinLee1215 <leejaejin1215@gmail.com>
2023-08-02 11:20:42 +0900

    test

--- demo/src/main/java/com/example/demo/menu/find/controller/MenuController.java
+++ demo/src/main/java/com/example/demo/menu/find/controller/MenuController.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import String
 
+import static com.fasterxml.jackson.databind.cfg.CoercionInputShape.String;
 
 @RestController
 @RequestMapping("menu/find")
